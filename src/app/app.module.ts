8ff54978d83a273901676413af4b135039395c31
vudu151 <109599042+vudu151@users.noreply.github.com>
2022-11-16 18:45:37 +0700

    up code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {RouterModule, Routes} from "@angular/router";
+import {CentresComponent} from "./centres/centres.component";
+import {DoctorsComponent} from "./doctors/doctors.component";
 
+const appRoutes: Routes=[
+  {path:'',component: AppComponent},
+  {path:'centres',component: CentresComponent},
+  {path:'doctors',component: DoctorsComponent}
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,CentresComponent,DoctorsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)   // cost: appRoutes
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/centres/centres.component.ts
@@ -0,0 +1,9 @@
+import {Component} from "@angular/core";
+
+@Component({
+  selector:'app-centres',
+  templateUrl:'centres.component.html',
+  styleUrls:['centres.component.css']
+})
+
+export class CentresComponent {}
