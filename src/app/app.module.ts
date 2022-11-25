0caba17527cc280c43e7ab2a8398b8b2e6453204
vudu151 <109599042+vudu151@users.noreply.github.com>
2022-11-25 07:31:00 +0700

    up code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import {RouterModule, Routes} from "@angular/router";
 import {DoctorsComponent} from "./doctors/doctors.component";
-import {ContactComponent} from "./contact/contact.component";
 import {CentresComponent} from "./centres/centres.component";
+import {ContactComponent} from "./contact/contact.component";
+import {ServicesComponent} from "./services/services.component";
 
 const appRoutes: Routes=[
   {path:'',component: AppComponent},
   {path:'doctors',component: DoctorsComponent},
   {path:'centres',component: CentresComponent},
-  {path:'contact',component: ContactComponent}
-
+  {path:'services',component: ServicesComponent},
+  {path:'contact',component: ContactComponent},
 ]
 @NgModule({
   declarations: [
-    AppComponent,DoctorsComponent,CentresComponent,ContactComponent
+    AppComponent,DoctorsComponent,CentresComponent,ContactComponent,ServicesComponent
   ],
   imports: [
     BrowserModule,
